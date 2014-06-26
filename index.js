5b6c39235afd389621046e94816047b8f8b40e9d
sethvincent <sethvincent@gmail.com>
2014-06-25 21:21:32 -0700

    refactor for heroku

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var Twit = require('twit');
-var config = require('./config.js');
-var twitter = new Twit(config);
+var twitter = new Twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
 var flatsheet = require('flatsheet')({
   token: process.env.FLATSHEET_TOKEN
 });
